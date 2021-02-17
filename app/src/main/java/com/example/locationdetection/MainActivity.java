e06da116cbf5d4c7b6fb4a5d9acc67d24fa08af8
Nehap-710 <nehapattankar@gmail.com>
2021-02-17 16:23:36 +0530

    Location Detection

---
+++ app/src/main/java/com/example/locationdetection/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.locationdetection;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    TextView Address;
+    Button button;
+    EditText Landmark , PlantName;
+    Editable land, plant;
+    LocationManager locationManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Address = findViewById(R.id.address);
+        button = findViewById(R.id.button);
+        Landmark = findViewById(R.id.landmark);
+        PlantName = findViewById(R.id.plantname);
+        land = Landmark.getText();
+        plant = PlantName.getText();
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+          != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION},100);
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation();
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation(){
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,3,MainActivity.this);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+            //Toast.makeText(this, ""+location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
+            try {
+                Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                String address= addresses.get(0).getAddressLine(0);
+                Address.setText(address);
+                boolean check = address.contains(land);
+                if(check == true){
+                    Toast.makeText(this, "Location is Verified", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this,coin.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(this, "Location not verified", Toast.LENGTH_SHORT).show();
+                }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+}
